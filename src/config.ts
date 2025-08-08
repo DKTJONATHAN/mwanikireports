d88179523fe83e6828721c5162e3665b3ee1e9d1
MysteryCodes <39903046+DKTJONATHAN@users.noreply.github.com>
2025-08-08 20:08:52 +0300

    Create config.ts

---
+++ src/config.ts
@@ -0,0 +1,15 @@
+export const SITE = {
+  title: "The Mwaniki's",
+  description: "Stories, Gossip, Tech, and News",
+  author: "Jonathan Mwaniki",
+  theme: "red-black",
+  domain: "JonathanMwaniki.co.ke",
+};
+
+export const CATEGORIES = [
+  "Stories",
+  "Trending",
+  "Gossip",
+  "News",
+  "Tech",
+];
